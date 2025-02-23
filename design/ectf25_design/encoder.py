01ce82b67bfab3b71c8bff165036f794a22d94a2
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-02-23 17:26:35 -0600

    Update encoder.py

--- design/ectf25_design/encoder.py
+++ design/ectf25_design/encoder.py
@@ -4,10 +4,12 @@ import json
 from cryptography.fernet import Fernet
 
 class Encoder:
-    def __init__(self, secrets: bytes, encryption_key: bytes):
+    def __init__(self, secrets: bytes, encryption_key: str):
         secrets = json.loads(secrets)
+        encryption_key = encryption_key.encode('utf-8')
         self.cipher = Fernet(encryption_key)
         self.some_secrets = secrets["some_secrets"]
+
 
     def encode(self, channel: int, frame: bytes, timestamp: int) -> bytes:
         data = struct.pack("<IQ", channel, timestamp) + frame
@@ -23,8 +25,8 @@ def main():
     parser.add_argument("timestamp", type=int, help="64b timestamp to use")
     args = parser.parse_args()
 
-    encoder = Encoder(args.secrets_file.read(), args.key_file.read())
+    encoder = Encoder(encryption_key=args.encryption_key, secrets=args.secrets.read())
     print(repr(encoder.encode(args.channel, args.frame.encode(), args.timestamp)))
-
+    #args.secrets_file.read(), args.key_file.read() old Encoder args
 if __name__ == "__main__":
     main()
